205aac702fc2c544c273d5152548c45daa2bb31b
dimitar-paskov <dimitarp2002@abv.bg>
2025-12-11 20:09:17 +0200

    Add backend provider

--- main.tf
+++ main.tf
@@ -11,6 +11,12 @@ terraform {
       version = "3.7.2"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstoragedpaskov"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
